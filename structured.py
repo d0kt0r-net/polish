669d09544fe4f68b2df1ea1f1a983febd0dd95e2
d0kt0r <work@d0kt0r.net>
2022-04-14 14:21:53 +0300

    fix errors

--- structured.py
+++ structured.py
@@ -1,30 +1,30 @@
 """ Polish calculator structured code """
 
 
-def filter_data(input_string: str) -> list:
-    valid_signs = '+-*/()'
+def filter_data(in_string: str) -> list:
+    valid_signs = '+-*/().'
 
     pre_sign_char = '+-*/('
     sign_char = '+-'
 
-    filtered_string = ''.join(filter(lambda c: c.replace('.', '').isdigit() or c in valid_signs, input_string))
+    filtered_string = ''.join(filter(lambda c: c.isdigit() or c in valid_signs, in_string))
     expression_list = list()
     prev_char = '('
     i = 0
 
     while i < len(filtered_string):
         cur_char = filtered_string[i]
-        if prev_char in pre_sign_char and cur_char in sign_char or cur_char.replace('.', '').isdigit():
+        if prev_char in pre_sign_char and cur_char in sign_char or cur_char.replace('.', '0').isdigit():
             j = 1
-            while i + j < len(filtered_string) and filtered_string[i + j].replace('.', '').isdigit():
+            while i + j < len(filtered_string) and filtered_string[i + j].replace('.', '0').isdigit():
                 j += 1
-            expression_list.append(int(filtered_string[i: i + j]))
+            expression_list.append(float(filtered_string[i: i + j]))
             i += j
             pass
         else:
             expression_list.append(cur_char)
             i += 1
-        prev_char = cur_char
+        prev_char = filtered_string[i - 1]
 
     return expression_list
 
@@ -70,16 +70,18 @@ def count_polish(input_expression: list) -> float:
     stack = list()
     for element in input_expression:
         if isinstance(element, str):  # operators
-            first_argument = stack.pop()
             second_argument = stack.pop()
+            first_argument = stack.pop()
             if element == '+':
                 operation_result = first_argument + second_argument
-            if element == '-':
+            elif element == '-':
                 operation_result = first_argument - second_argument
-            if element == '*':
+            elif element == '*':
                 operation_result = first_argument * second_argument
-            if element == '/':
+            elif element == '/':
                 operation_result = first_argument / second_argument
+            else:
+                raise KeyError
             stack.append(operation_result)
         else:
             stack.append(element)
@@ -102,4 +104,3 @@ if __name__ == '__main__':
 
         except EOFError:
             break
-
